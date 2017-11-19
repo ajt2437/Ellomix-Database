0c408543903e62217a73f97d35b4b41eda54bff1
stevenvill <stevenvillarreal8@gmail.com>
2017-11-18 19:57:03 -0600

    updated script to run with no errors

--- create_tables.sql
+++ create_tables.sql
@@ -1,14 +1,14 @@
-\c postgres
+\c postgres;
 drop database if exists ellomix;
 create database ellomix;
 \c ellomix;
 
-create table User(
+create table Users(
 	user_id serial primary key not null unique,
 	name varchar(75) not null,
 	description varchar(500),
 	followers_count int,
-	following count int,
+	following_count int,
 	photo_url text
 );
 
@@ -28,26 +28,26 @@ create table Playlist(
 
 create table TimelinePost(
 	post_id serial primary key not null unique,
-	user_id serial references User(user_id),
+	user_id serial references Users(user_id),
 	track_id serial references Track(track_id),
-	date_posted timestampz,
+	date_posted timestamp with time zone,
 	description text
 );
 
 create table Comment(
-	user_id serial references User(user_id),
+	user_id serial references Users(user_id),
 	comment varchar(500)
 );
 
 create table Relationship(
-	follower_id serial primary key not null unique, #????
-	user_id serial references User(user_id)
+	follower_id serial primary key not null unique,
+	user_id serial references Users(user_id)
 );
 
 create table Message(
-	message_id primary key not null unique,
+	message_id serial primary key not null unique,
 	message text,
-	user_id serial references User(user_id),
+	user_id serial references Users(user_id),
 	photo_url text
 );
 
@@ -59,8 +59,8 @@ create table Chat(
 	message_id serial references Message(message_id)
 );
 
-create table Chat_User(
+create table Chat_Users(
 	chat_user_id serial primary key not null unique,
-	user_id serial references User(user_id),
+	user_id serial references Users(user_id),
 	chat_id serial references Chat(chat_id)
 );
