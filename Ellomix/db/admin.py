fffe4c9dbf54cc9833f495c048e5f78b2f1fe38f
stevenvill <stevenvillarreal8@gmail.com>
2018-02-12 20:45:13 -0600

    Registered models inthe Django admin page

--- Ellomix/db/admin.py
+++ Ellomix/db/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Users)
+admin.site.register(Track)
+admin.site.register(Playlist)
+admin.site.register(TimelinePost)
+admin.site.register(Comment)
+admin.site.register(Relationship)
+admin.site.register(Message)
+admin.site.register(Chat)
+admin.site.register(ChatUsers)
